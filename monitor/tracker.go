e87a93337ddb28c0fdd8f1a8bf0583d6f04b6024
Alfonso Subiotto Marqués <alfonso.subiotto.marques@gmail.com>
2016-03-25 10:51:30 -0400

    Working on log parsing

--- monitor/tracker.go
+++ monitor/tracker.go
@@ -2,28 +2,30 @@
 package monitor
 
 import (
-	"container/heap"
 	"errors"
-	"fmt"
 	"strings"
 	"sync"
 )
 
 // MonitorNode represents a section of our website. All MonitorNodes are kept
-// track of in a PriorityQueue.
+// track of in a HitList.
 type MonitorNode struct {
 	section string
 	hits    int // How many times this section has been visited.
 	index   int // Used by the priority queue.
 }
 
-// A PriorityQueue implements heap.Interface and holds MonitorNodes.
-type PriorityQueue []*MonitorNode
+// HitList is an array of sorted MonitorNodes. We do not use a priority queue
+// because it is easier to get the n first elements from a sorted array. To
+// get n elements at read time from the priority queue one must remove each
+// element then reinsert after all elements have been read to produce the
+// correct order.
+type HitList []*MonitorNode
 
-// Tracker is a wrapper struct around a PriorityQueue providing several
+// Tracker is a wrapper struct around a HitList providing several
 // middleware methods for keeping track of HTTP access logs.
 type Tracker struct {
-	pq *PriorityQueue
+	hitList *HitList
 }
 
 var (
@@ -33,40 +35,33 @@ var (
 	trackerMtx           sync.RWMutex
 )
 
-func (pq PriorityQueue) Len() int { return len(pq) }
-
-func (pq PriorityQueue) Less(i, j int) bool {
-	// We want Pop to give us the highest, not lowest, hits so we use greater
-	// than here.
-	return pq[i].hits > pq[j].hits
-}
-
-func (pq PriorityQueue) Swap(i, j int) {
-	pq[i], pq[j] = pq[j], pq[i]
-	pq[i].index = i
-	pq[j].index = j
-}
-
-func (pq *PriorityQueue) Push(x interface{}) {
-	n := len(*pq)
-	node := x.(*MonitorNode)
-	node.index = n
-	*pq = append(*pq, node)
-}
-
-func (pq *PriorityQueue) Pop() interface{} {
-	old := *pq
-	n := len(old)
-	node := old[n-1]
-	node.index = -1 // for safety
-	*pq = old[0 : n-1]
+// InsertSection inserts a new node into the HitList with the specified section
+// and a hit counter of 1 and returns it.
+func (hl *HitList) InsertSection(section string) *MonitorNode {
+	node := &MonitorNode{
+		section: section,
+		index:   len(*hl),
+	}
+	*hl = append(*hl, node)
+	hl.Update(node, 1) // Update takes the number of hits.
 	return node
 }
 
-// update modifies the hits of a MonitorNode.
-func (pq *PriorityQueue) update(node *MonitorNode, hits int) {
+// Update modifies the hits of a MonitorNode and swaps until the list is sorted.
+func (hl *HitList) Update(node *MonitorNode, hits int) {
 	node.hits = hits
-	heap.Fix(pq, node.index)
+	curIndex := node.index
+
+	list := *hl
+	for {
+		if curIndex == 0 || list[curIndex-1].hits >= list[curIndex].hits {
+			break
+		}
+
+		list[curIndex-1], list[curIndex] = list[curIndex], list[curIndex-1]
+		list[curIndex].index = curIndex
+		list[curIndex-1].index = curIndex - 1
+	}
 }
 
 // GetTracker initializes the global tracker if it has not been initialized yet
@@ -76,9 +71,8 @@ func GetTracker() *Tracker {
 		trackerMtx.Lock()
 		defer trackerMtx.Unlock()
 
-		pq := make(PriorityQueue, 0)
-		heap.Init(&pq)
-		tracker = &Tracker{pq: &pq}
+		hl := make(HitList, 0)
+		tracker = &Tracker{hitList: &hl}
 	}
 
 	return tracker
@@ -91,10 +85,10 @@ func GetTracker() *Tracker {
 func (t *Tracker) ProcessLogEntry(logEntry string) error {
 	// HTTP access logs seem to vary quite a lot depending on the server
 	// logging the accesses. The unique element across all access logs is that
-	// the request line is contained within quotes and always contains the
-	// HTTP method.
+	// the HTTP method is always contained and occurs after the first \" char.
 	values := strings.Split(logEntry, "\"")
-	if len(values) != 3 {
+	if len(values) < 2 {
+		print(logEntry)
 		return errMalformedLogEntry
 	}
 
@@ -104,13 +98,18 @@ func (t *Tracker) ProcessLogEntry(logEntry string) error {
 	if len(pathComponents) > 1 {
 		section = pathComponents[1]
 	} else {
-		fmt.Println(pathComponents)
-		fmt.Println(logEntry)
+		print(logEntry)
 		return errMalformedLogEntry
 	}
 
-	if section == "" {
-		// We split "/" into ["", ""].
+	if len(pathComponents[1]) < 1 {
+		// There was nothing after the first slash.
+		return errMalformedLogEntry
+	}
+
+	// TODO(asubiotto): There are some weird access log things.
+	if string(pathComponents[1][0]) == " " {
+		// If there was a space, our section was the root.
 		section = "/"
 	}
 
@@ -119,14 +118,9 @@ func (t *Tracker) ProcessLogEntry(logEntry string) error {
 	trackerMtx.Lock()
 	defer trackerMtx.Unlock()
 	if node, ok := sections[section]; ok {
-		t.pq.update(node, node.hits+1)
+		t.hitList.Update(node, node.hits+1)
 	} else {
-		node = &MonitorNode{
-			section: section,
-			hits:    1,
-		}
-		sections[section] = node
-		heap.Push(t.pq, node)
+		sections[section] = t.hitList.InsertSection(section)
 	}
 
 	return nil
@@ -138,20 +132,15 @@ func (t *Tracker) GetTopHits(limit int) []MonitorNode {
 	trackerMtx.RLock()
 	defer trackerMtx.RUnlock()
 
-	// Copy priority queue.
-	pq := *t.pq[:]
-	result := []MonitorNode{}
-
-	for ; limit > 0; limit-- {
-
+	if limit > len(*t.hitList) {
+		limit = len(*t.hitList)
 	}
 
-	if limit == 0 {
-		return []*MonitorNode{}
-	} else if limit >= len(*t.pq) {
-		return (*t.pq)[:]
+	// Don't return pointers for safety.
+	result := make([]MonitorNode, limit)
+	for i := 0; i < limit; i++ {
+		result[i] = *((*t.hitList)[i])
 	}
 
-	return (*t.pq)[len(*t.pq)-limit:]*/
 	return result
 }
